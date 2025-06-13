39725bf7c9c1757b4ee7e89d805fd2e0820d3a4d
AliaksandrNazaruk <46998499+AliaksandrNazaruk@users.noreply.github.com>
2025-06-13 15:14:28 +0200

    Update protocol.py

--- protocol.py
+++ protocol.py
@@ -1,7 +1,7 @@
 """
 protocol.py — универсальный SDO-протокол для dryve-D1, работающий поверх transport.py и packet.py
 
-© 2025 Your-Company / MIT-license
+© 2025 Aliaksandr Nazaruk / MIT-license
 """
 
 import time
